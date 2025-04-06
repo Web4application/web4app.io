6b84897f8e87fad7e75a9c99963a113aa39eab39
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-06 13:29:24 -0400

    Create Recommendation.go

---
+++ services/Recommendation.go
@@ -0,0 +1,8 @@
+package services
+
+import "math/rand"
+
+func GetRecommendations(userID int) []int {
+    // Placeholder logic for recommendations
+    return []int{rand.Intn(100), rand.Intn(100), rand.Intn(100)}
+}
