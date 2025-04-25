9195cc69d7dcf02d95f229c9d836cc3f699323a4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-24 23:42:40 -0700

    Update Install.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Install.sh
+++ Install.sh
@@ -1,3 +1,4 @@
 npm install @liveblocks/client @liveblocks/yjs yjs codemirror @codemirror/lang-javascript y-codemirror.next
 npm i @vercel blob
 npm init builder.io@latest
+installer -pkg swiftly-1.0.0.pkg -target CurrentUserHomeDirectory
