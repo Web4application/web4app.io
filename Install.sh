a8b9aecac1b884126d3459a4b56e72874e8cd572
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 02:14:28 -0700

    Update Install.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Install.sh
+++ Install.sh
@@ -2,3 +2,7 @@ npm install @liveblocks/client @liveblocks/yjs yjs codemirror @codemirror/lang-j
 npm i @vercel blob
 npm init builder.io@latest
 installer -pkg swiftly-1.0.0.pkg -target CurrentUserHomeDirectory
+npx create-react-app discord-clone
+cd discord-clone
+npm install socket.io-client react-router-dom
+
