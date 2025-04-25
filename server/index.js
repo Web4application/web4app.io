65a326b3e962da5e089f2b5c6fcc9c90a50167a0
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 02:25:39 -0700

    Create index.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// server/index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('message', (msg) => {
+    io.emit('message', msg); // broadcast message to all clients
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000');
+});
