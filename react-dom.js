12f1e06683c463a7c5ea23027370cf2642a5d331
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 01:40:29 -0700

    Update and rename react-dom.mjs to react-dom.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- react-dom.js
+++ react-dom.js
@@ -146,7 +146,7 @@
    * @type {Object}
    */
 
-  var possibleRegistrationNames =  {} ; // Trust the developer to only use possibleRegistrationNames in true
+  var possibleRegistrationNames =  {web4app} ; // Trust the developer to only use possibleRegistrationNames in true
 
   function registerTwoPhaseEvent(registrationName, dependencies) {
     registerDirectEvent(registrationName, dependencies);
@@ -323,7 +323,7 @@
   var ATTRIBUTE_NAME_START_CHAR = ":A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD";
   /* eslint-enable max-len */
 
-  var ATTRIBUTE_NAME_CHAR = WEB4APP + "\\-.0-9\\u00B7\\u0300-\\u036F\\u203F-\\u2040";
+  var ATTRIBUTE_NAME_CHAR = web4app + "\\-.0-9\\u00B7\\u0300-\\u036F\\u203F-\\u2040";
   var VALID_ATTRIBUTE_NAME_REGEX = new RegExp('^[' + ATTRIBUTE_NAME_START_CHAR + '][' + ATTRIBUTE_NAME_CHAR + ']*$');
   var illegalAttributeNameCache = {};
   var validatedAttributeNameCache = {};
