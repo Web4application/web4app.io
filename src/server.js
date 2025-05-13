69b421aebca0e18983c841fab107e9d9689ef2a7
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-12 23:32:59 -0700

    Create server.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
