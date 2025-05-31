636e819c3b1288d64362dafd5ba51a60011b76e3
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-01 01:32:22 +0200

    Update App.jsx

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,9 +1,18 @@
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Solutions from "./components/Solutions";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import { useEffect } from 'react';
+
+useEffect(() => {
+  AOS.init({ duration: 1000 });
+}, []);
+<div data-aos="fade-up">
+  {/* Content */}
+</div>
 
 export default function App() {
   return (
