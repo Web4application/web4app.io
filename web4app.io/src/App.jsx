c2ec06effa7710bb62dbce39d901274c298c6b63
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-26 22:46:15 +0200

    Add files via upload

---
+++ web4app.io/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Solutions from "./components/Solutions";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Solutions />
+      <About />
+      <Footer />
+    </>
+  );
+}
