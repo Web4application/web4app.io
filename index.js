c5a740683071a8a9bc13e9adaba5c96393ab4eee
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-04 20:04:24 +0100

    Update and rename .production.min.js to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function (global, factory) {
+( function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react')) :
   typeof define === 'function' && define.amd ? define(['exports', 'react'], factory) :
   (global = global || self, factory(global.ReactDOM = {}, global.React));
