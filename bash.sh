c0cd0e9652d7dfecb99bc62932d664465d6fb64d
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-02 03:11:38 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,7 +8,7 @@ if [ "$1" = "--debug" ]; then
 fi
 
 if [ -n "$NODENV_DEBUG" ]; then
-  # https://wiki-dev.bash-hackers.org/scripting/debuggingtips#making_xtrace_more_useful
+  
   export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
   set -x
 fi
@@ -193,3 +193,18 @@ cmake --build build
 sudo cmake --install build
 
 
+set -e
+rm -rf ./vendor/cmd/go
+cp -a $(go env GOROOT)/src/cmd/go vendor/cmd/go
+rm -f vendor/cmd/go/alldocs.go vendor/cmd/go/mkalldocs.sh # docs are in wrong place and describe wrong command
+cd vendor/cmd/go
+patch -p0 < ../../../patch.txt
+vers=$(go version | sed 's/^go version //; s/ [A-Z][a-z][a-z].*//')
+echo "package version; const version = \"$vers\"" > internal/version/vgo.go
+gofmt -w internal
+cd ../../..
+rm $(find . -name '*.orig')
+go build
+./vgo version
+rm vgo
+git add .
